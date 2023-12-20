7a2997746f403b2bfbf9a7210743b24b776fca5b
Andressa <andressa.sena@mutua.com.br>
2023-12-20 17:23:30 -0300

    Adicionando Conteúdo

--- Banco_Dados/DemandaZe.sql
+++ Banco_Dados/DemandaZe.sql
@@ -1,6 +1,6 @@
-Alguns scripts que o Zé me ensinou quando me passou a demanda de envio de e-mails. 
+--Alguns scripts que o Zé me ensinou quando me passou a demanda de envio de e-mails. 
 
--> Criação do Banco de Dados e da Tabela
+--Criação do Banco de Dados e da Tabela
 CREATE DATABASE IF NOT EXISTS `envio_certificados`;
 USE `envio_certificados`;
 
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS `relatorio_certificados_profissional` (
   PRIMARY KEY (`ID_RELATORIO`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--> Inserir dados no Banco
+--Inserir dados no Banco
 INSERT INTO `relatorio_certificados_profissional` (`ID_RELATORIO`, `N_DO_ARQUIVO`, `CERTIFICADO`, `NOME`, `CPF`, `NOME_DO_ARQUIVO`, `E_MAIL`, `ENVIADO`) VALUES
 	(1, 1, 1, 'Nome1', '12180159715', 'arquivo1', 'email1', 0),
 	(2, 2, 2, 'Nome2', '98952572372', 'arquivo2', 'email2', 0);
 
--> Requisições Diversas
+--Requisições Diversas
 SELECT COUNT(*) FROM relatorio_apolice_global_certificados_rc_profissional WHERE ENVIADO=0;
 
 SELECT NOME, NOME_DO_ARQUIVO, LOWER(E_MAIL), ID_RELATORIO FROM relatorio_apolice_global_certificados_rc_profissional WHERE (E_MAIL is not NULL) AND (ENVIADO = 0) AND (ID_RELATORIO BETWEEN 1101 AND 1200);
 
-SELECT * FROM relatorio_apolice_global_certificados_rc_profissional WHERE E_MAIL = 'thiagohmelo.93@gmail.com'
+SELECT * FROM relatorio_apolice_global_certificados_rc_profissional WHERE E_MAIL = 'exemploaleatorio@gmail.com'
