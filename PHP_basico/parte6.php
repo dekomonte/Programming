e0ed0729a6820db8a4c190c25d49382bbe7dcd21
Andressa <andressa.sena@mutua.com.br>
2023-12-20 16:51:48 -0300

    Adicionando Conteúdo

--- PHP_basico/parte6.php
+++ PHP_basico/parte6.php
@@ -5,7 +5,21 @@
 /*---------------Declaração de Funções---------------*/
 /*Exemplo: Conexão com Banco de Dados
 
+function conexao()
+{
+    $pdo = nem PDO('mysql:host=localhost; dbname=books','root','');
+    return $pdo;
+}
 
+function getData()
+{
+    $conexao = conexao();
+    $query = $conexao->query('select * from users');
+    $query->execute();
+    return $query->fetchAll();
+}
+
+var_dump(getData());
 
 */
 
