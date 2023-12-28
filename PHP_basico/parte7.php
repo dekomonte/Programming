791f16fe97cb4280b6b07279a52c078f69c1f540
Andressa <andressa.sena@mutua.com.br>
2023-12-28 16:11:00 -0300

    Adicionando Conteúdo de PHP

--- PHP_basico/parte7.php
+++ PHP_basico/parte7.php
@@ -3,5 +3,36 @@
 /*---------------PHP: PARTE 7---------------*/
 
 /*---------------Closures---------------*/
+#Funções anônimas
+#Objeto do tipo closure
+#Precisa colocar ; ao final da função 
+$person = function(){
+    return 'teste';
+    echo "\n";
+};
+
+#Colocar () pra trabalhar com o método invoke
+var_dump($person());
+echo "\n";
+
+/*---------------Callbacks---------------*/
+#Funções passadas como parâmetro para outras funções
+function teste(){
+    echo "\n";
+    return 'teste';
+}
+
+function teste2($callback){
+    return $callback();
+}
+#Passar a função como string
+echo teste2('teste');
+
+function f1($nome){
+    echo "\n";
+    return 'Olá meu nome é ' . $nome; 
+}
+
+echo call_user_func('f1','Andressa');
 
 ?>
