de27166a816d805b12ab172be3e76809bf3b6829
Andressa <andressa.sena@mutua.com.br>
2024-05-06 09:33:36 -0300

    Update Banco de Dados

--- Banco_Dados/Script_curso_resumo.sql
+++ Banco_Dados/Script_curso_resumo.sql
@@ -46,3 +46,37 @@ SELECT COUNT(*) AS "Quantidade de Registros da Tab. Cliente" FROM CLIENTE;
 
 -- Group by
 SELECT SEXO, COUNT(*) FROM CLIENTE GROUP BY SEXO;
+
+----------#----------#----------#----------#----------
+--RESUMO - INNER JOIN
+
+--Combina dados de várias tabelas
+--Permite a junção entre duas ou mais tabelas, desde que haja entrelaçamento entre todas
+--Pensando na teoria de conjuntos, é a interseção entre dois conjuntos
+
+--Exemplo:
+SELECT
+    P.nome,
+    P.preco,
+    C.nome as Categoria
+FROM
+    produto P
+INNER JOIN
+    categoria_produto C
+ON P.id_categoria = C.id;
+
+--Exemplo - Várias tabelas: 
+SELECT
+    P.nome,
+    P.preco,
+    C.nome as Categoria,
+    COUNT(V.id_produto) TOTAL_VENDIDOS
+FROM
+    produto P
+INNER JOIN
+  categoria_produto C
+ON P.id_categoria = C.id
+INNER JOIN
+  venda_produto V
+ON V.id_produto = P.id
+GROUP BY P.id;
